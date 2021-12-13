942a27ba753ca3c91f35435588183bd3084faca0
billy-yjh <40520384+billy-yjh@users.noreply.github.com>
2021-12-13 17:44:41 +0800

    Add files via upload

---
+++ newOne.py
@@ -0,0 +1,128 @@
+# !/user/bin/ env python -*- coding:UTF-8 -*-
+# sieve = [True] * 101
+# for i in range(2, 100):
+#     if sieve[i]:
+#         print(i)
+#         for j in range(i*i, 100, i):
+#             sieve[j] = False
+
+# print('hello world')
+
+# print(None == 0)
+
+# print(3.1415926)
+
+# print(type(3.1415926))
+# print(type('Learn Python in imooc.'))
+
+# a = 'hello world'
+# b = a
+# print(b)
+
+# longRow = 3.14
+# shortRow = 1.57
+# rounds = longRow * shortRow
+
+# rounds = round(rounds,2)
+# print(rounds)
+
+
+# print(10//3)
+
+# print(10 / 3)
+
+# print(True and False)
+
+# a = 'python'
+# print('hello', a or 'world')
+
+# b = ''
+# print('hello', b or 'world')
+
+# print("hello's world \" me")
+
+# r'\(~_~) this is a book'
+
+# r'\(~_~)/ \(~_~)/'
+
+# print(r'''To be, or not to be \":that is a question.
+#  Whether it\'s nobler in the mind to suffer''')
+
+# print(r'''Python is created by "Guido".
+# It is free and easy to learn.
+# Let's start learn Python in imooc!''')
+
+
+# template = 'hello {}'
+# world = 'World'
+# result = template.format(world)
+# print(result)
+# print('{1} {0}'.format('this is a template', 'get'))
+
+# template1 = 'life is short'
+# template2 = 'you need go on'
+# print('{} {}'.format(template1, template2))
+# print('{1}, {0}'.format(template2, template1))
+
+# s = 'ABC'
+# a = s[0]
+# b = s[1]
+# c = s[2]
+# print(a)
+# print(b)
+# print(c)
+
+# a =  'hello this is a good boy loving you'
+# print(a[3:17])
+
+# temp = 12
+
+# if temp < 18:
+#     print('他是未成年人，年龄{}'.format(temp))
+# else:
+#     print(temp)
+
+# temp = 17
+
+# if temp < 17:
+#     temp += 1
+#     print('明年年龄为()'.format(temp))
+# else:
+#     if temp == 17:
+#         key = 3
+#         i = 0
+#         while i < key:
+#             i = i + 1
+#             temp += 1
+#         print(temp)
+#     else:
+#         print(temp)
+
+
+# s = 'asdadads'
+
+# for i in s:
+#     print(i)
+
+# i =  0
+# count = 10
+# while  True:
+#     if i < 100:
+#         i += 1
+#         count = count + i
+#     else:
+#         print(count)
+#         break
+
+# num = 1
+# sum =  0
+# while True:
+#     if num <=100:
+#         sum = num + sum
+#         num =  num  + 1
+#     else:
+#         print('0到100的和是{}'.format(sum,2))
+#         break
+
+
+
